2990c5d6f136f39ad20ec5aa11c7d799e4165b21
lv-wei <13366824@qq.com>
2021-09-03 17:20:44 +0800

    use ? for getting content

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -9,14 +9,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    let result = std::fs::read_to_string(&args.path);
-
-    let content = match result {
-        Ok(content) => content,
-        Err(error) => {
-            panic!("Can't deal with {}, just exit here", error);
-        }
-    };
+    let content = std::fs::read_to_string(&args.path)?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
