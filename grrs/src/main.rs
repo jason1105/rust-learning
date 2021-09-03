f34b177fa8025981d26e7cf1e6d4b8ad0162a65b
lv-wei <13366824@qq.com>
2021-09-03 17:17:30 +0800

    use match to get content

---
+++ grrs/src/main.rs
@@ -0,0 +1,28 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+    let result = std::fs::read_to_string(&args.path);
+
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Can't deal with {}, just exit here", error);
+        }
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
