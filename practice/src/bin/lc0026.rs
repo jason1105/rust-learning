7c7a23629c31b35d48e16cbba7c8f2ea036bab60
lv-wei <13366824@qq.com>
2021-09-10 23:28:35 +0800

    lc0026 version 3

--- practice/src/bin/lc0026.rs
+++ practice/src/bin/lc0026.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let mut nums = vec![1, 1, 1];
+    let mut nums = vec![];
     let ans = remove_duplicates(&mut nums);
-    println!("{}", ans);
+    println!("{} {:?}", ans, nums);
 }
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    nums.dedup();
-    nums.len() as i32
+    nums.sort();
+    match nums.is_empty() {
+        true => return 0,
+        _ => {
+            let mut i = 0;
+            for j in 1..nums.len() {
+                if nums[i] != nums[j] {
+                    nums[i + 1] = nums[j];
+                    i += 1;
+                }
+            }
+            (i + 1) as i32
+        }
+    }
 }
