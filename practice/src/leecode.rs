1fb7ecf4c6cd5355808b84aec2131ce878a8c666
lv-wei <13366824@qq.com>
2021-09-25 22:00:32 +0800

    lc0146 LRU

--- practice/src/leecode.rs
+++ practice/src/leecode.rs
@@ -1,3 +1,4 @@
 pub mod lc0058;
 pub mod lc0067;
 pub mod lc0125;
+pub mod lc0146;
--- practice/src/main.rs
+++ practice/src/main.rs
@@ -22,3 +22,34 @@ fn main() {
     let b = Vec::from(b);
     println!("{}", a[0] - b'0' + b[1]);
 }
+mod test {
+    use std::{pin::Pin, task::Poll};
+
+    use futures_io::{AsyncBufRead, AsyncRead};
+
+    //不能通过编译
+    struct Body(Vec<u8>);
+    impl AsyncRead for Body {
+        fn poll_read(
+            self: Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+            buf: &mut [u8],
+        ) -> Poll<futures_io::Result<usize>> {
+            todo!()
+        }
+    }
+
+    impl AsyncBufRead for Body {
+        fn poll_fill_buf(
+            self: Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> Poll<futures_io::Result<&[u8]>> {
+            // Poll::Ready(Ok(&self.0))
+            Poll::Ready(Ok(&(Pin::into_inner(self).0)))
+        }
+
+        fn consume(self: Pin<&mut Self>, amt: usize) {
+            todo!()
+        }
+    }
+}
