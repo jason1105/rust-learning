4cfa7dbb12cc0d52ec8ec17cce200c68ccc61fb6
lv-wei <13366824@qq.com>
2022-09-29 16:04:20 +0800

    Add example

--- code_of_The_Book/15-Smart_pointers/01-box/smart_pointer_box/src/main.rs
+++ code_of_The_Book/15-Smart_pointers/01-box/smart_pointer_box/src/main.rs
@@ -14,8 +14,7 @@ fn main() {
     use List::{Cons, Nil};
 
     // Listing 15-3: Using the List enum to store the list 1, 2, 3
-    let list = Cons(1,Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     // Listing 15-6: Using the dereference operator to follow a reference to an i32 value
     let x = 5;
@@ -43,9 +42,10 @@ fn main() {
     use std::ops::Deref;
 
     impl<T> Deref for MyBox<T> {
-        type Target = T;  // Associated type covered in Chapter 19
+        type Target = T; // Associated type covered in Chapter 19
 
-        fn deref(&self) -> &Self::Target { // Self::Target means Deref::Target, the type of return value must be a reference. so add &.
+        fn deref(&self) -> &Self::Target {
+            // Self::Target means Deref::Target, the type of return value must be a reference. so add &.
             &self.0 // we need to return value of type of reference, so add &
         }
     }
@@ -65,7 +65,6 @@ fn main() {
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
 
-    
     let m = MyBox::new(String::from("Rust"));
     hello(&(*m)[..]);
 
@@ -73,7 +72,7 @@ fn main() {
     struct CustomSmartPointer {
         data: String,
     }
-    
+
     impl Drop for CustomSmartPointer {
         fn drop(&mut self) {
             println!("Dropping CustomSmartPointer with data `{}`!", self.data);
@@ -91,113 +90,163 @@ fn main() {
     };
     println!("CustomSmartPointers created.");
 
+    // We have to add lifetime when we impl Cons use reference
+    {
+        #[derive(Debug)]
+        enum List2<'a> {
+            Cons(i32, Box<&'a List2<'a>>),
+            Nil,
+        }
 
+        use List2::{Cons, Nil};
+
+        let x = Cons(10, Box::new(&Nil));
+        let a = Cons(5, Box::new(&x));
+        let b = Cons(3, Box::new(&a));
+        let c = Cons(4, Box::new(&a));
+        println!("{:?}", b);
+        println!("{:?}", c);
+    }
+
+    // while using Rc, compiler drop value depending on count number of Rc instead of lifetime
+    // Code seems more elegant.
+    {
+        #[derive(Debug)]
+        enum List3 {
+            Cons(i32, Rc<List3>),
+            Nil,
+        }
+
+        use std::rc::Rc;
+        use List3::{Cons, Nil};
+
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        let b = Cons(3, Rc::clone(&a));
+        let c = Cons(4, Rc::clone(&a));
+        println!("{:?}", b);
+        println!("{:?}", c);
+    }
+
+    {
+        let mut c = "asdf";
+        let one = c;
+        c = "qwer";
+        let two = c;
+        println!("one: {}, two: {}", one, two);
+    }
+
+    {
+        use std::cell::Cell;
+
+        struct S {
+            greet: Cell<&'static str>,
+        }
+
+        let s = S {
+            greet: Cell::new("hello"),
+        };
+
+        let mut v = s.greet.get();
+        s.greet.set("hello, world");
+    }
 }
 
 #[allow(unused_variables, unused_mut, unused_assignments)]
 #[cfg(test)]
 mod tests {
-        // Listing 15-17: Demonstrating we’re not allowed to have two lists using Box<T> that try to share ownership of a third list
-        // enum List {
-        //     Cons(i32, Box<List>),
-        //     Nil,
-        // }
-        // use List::{Cons, Nil};
-        // fn test1() {
-        //     let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-        //     let b = Cons(3, Box::new(a));
-        //     let c = Cons(4, Box::new(a));
-        // }
+    // Listing 15-17: Demonstrating we’re not allowed to have two lists using Box<T> that try to share ownership of a third list
+    // enum List {
+    //     Cons(i32, Box<List>),
+    //     Nil,
+    // }
+    // use List::{Cons, Nil};
+    // fn test1() {
+    //     let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    //     let b = Cons(3, Box::new(a));
+    //     let c = Cons(4, Box::new(a));
+    // }
 
-        // Listing 15-18: A definition of List that uses Rc<T>
+    // Listing 15-18: A definition of List that uses Rc<T>
 
-        use std::rc::Rc;
-    
-        enum List {
-            Cons(i32, Rc<List>),
-            Nil,
-        }
-        use List::{Cons, Nil};
+    use std::rc::Rc;
 
-        #[test]
-        fn test2() {
-            let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-            let b = Cons(3, Rc::clone(&a));
+    enum List {
+        Cons(i32, Rc<List>),
+        Nil,
+    }
+    use List::{Cons, Nil};
+
+    #[test]
+    fn test2() {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        let b = Cons(3, Rc::clone(&a));
+        let c = Cons(4, Rc::clone(&a));
+    }
+
+    // Listing 15-19: Printing the reference count
+    #[test]
+    fn test15_19() {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        println!("count after creating a = {}", Rc::strong_count(&a));
+        let b = Cons(3, Rc::clone(&a));
+        println!("count after creating b = {}", Rc::strong_count(&a));
+        {
             let c = Cons(4, Rc::clone(&a));
+            println!("count after creating c = {}", Rc::strong_count(&a));
+        }
+        println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    }
 
+    // test3 和 test4 比较一下
+    #[test]
+    fn test3() {
+        use std::rc::Rc;
+
+        #[derive(Debug)]
+        struct Student {
+            name: String,
+            class: Rc<String>, // 使用 Rc 引用
         }
 
-        // Listing 15-19: Printing the reference count
-        #[test]
-        fn test15_19() {
-            let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-            println!("count after creating a = {}", Rc::strong_count(&a));
-            let b = Cons(3, Rc::clone(&a));
-            println!("count after creating b = {}", Rc::strong_count(&a));
-            {
-                let c = Cons(4, Rc::clone(&a));
-                println!("count after creating c = {}", Rc::strong_count(&a));
-            }
-            println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+        let class = Rc::new(String::from("math"));
+        let lee = Student {
+            name: "lee".to_string(),
+            class: Rc::clone(&class),
+        };
+        let bob = Student {
+            name: "bob".to_string(),
+            class: Rc::clone(&class),
+        };
+
+        another(class.clone()); // Rc 的 clone 指的是克隆所有权的意思.
+
+        println!("lee: {:?}", lee);
+
+        fn another(class: Rc<String>) {}
+    }
+
+    #[test]
+    fn test4() {
+        #[derive(Debug)]
+        struct Student<'a> {
+            name: String,
+            class: &'a String, // 没使用Rc, 使用普通的引用
         }
 
-        // test3 和 test4 比较一下
-        #[test]
-        fn test3 () {
-            use std::rc::Rc;
-
-            #[derive(Debug)]
-            struct Student {
-                name: String,
-                class: Rc<String> // 使用 Rc 引用
-            }
-            
-            let class = Rc::new(String::from("math"));
-            let lee = Student{
-                    name: "lee".to_string(),
-                    class: Rc::clone(&class)
-                };
-            let bob = Student {
-                    name: "bob".to_string(),
-                    class: Rc::clone(&class)
-                };
-
-            another(class.clone()); // Rc 的 clone 指的是克隆所有权的意思.
-
-            println!("lee: {:?}", lee);
-
-            fn another(class: Rc<String>) {
-    
-            }
-
-        }
-
-        #[test]
-        fn test4() {
-            #[derive(Debug)]
-            struct Student<'a> {
-                name: String,
-                class: &'a String // 没使用Rc, 使用普通的引用
-            }
-
-            fn new_student(name: String, class: &String) -> Student {
-                Student {
-                    name: name,
-                    class: class
-                }
-            }
-
-            let class = String::from("math");
-            let lee = new_student("lee".to_string(), &class);
-            let bob = new_student("bob".to_string(), &class);
-            // another(class); // class 一旦被销毁, 下面的 lee 便无法引用了.
-            println!("lee: {:?}", lee);
-
-            #[allow(dead_code)]
-            fn another(class: String) {
-    
+        fn new_student(name: String, class: &String) -> Student {
+            Student {
+                name: name,
+                class: class,
             }
         }
 
+        let class = String::from("math");
+        let lee = new_student("lee".to_string(), &class);
+        let bob = new_student("bob".to_string(), &class);
+        // another(class); // class 一旦被销毁, 下面的 lee 便无法引用了.
+        println!("lee: {:?}", lee);
 
+        #[allow(dead_code)]
+        fn another(class: String) {}
+    }
 }
--- snippets/src/main.rs
+++ snippets/src/main.rs
@@ -1,11 +1,265 @@
-use std::{fmt, mem};
+use std::{fmt, mem, vec};
 
 use snippets::list::first;
 
 /// https://rust-unofficial.github.io/too-many-lists/first-pop.html
 ///
 
+pub fn reorder_spaces(text: String) -> String {
+    let arr = text
+        .split_ascii_whitespace()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>();
+    print!("{:?}", arr);
+    print!("{:?}", arr.join("--"));
+    let (word_cnt, space_cnt) = (
+        arr.len(),
+        text.as_bytes().iter().filter(|x| **x == ' ' as u8).count(),
+    );
+    let repeat = if word_cnt <= 1 {
+        0
+    } else {
+        space_cnt / (word_cnt - 1)
+    };
+    format!(
+        "{}{}",
+        arr.join(" ".repeat(repeat).as_str()),
+        " ".repeat(space_cnt - repeat * (word_cnt - 1))
+    )
+}
+
 fn main() {
-    let x: Option<String> = Some("hey".to_owned());
-    assert_eq!(x.as_deref(), Some("hey"));
+    let v1 = Some(1);
+    let v2 = v1.map(|v| 2);
+    assert_eq!(v2, Some(2));
+
+    let v1 = Some(1);
+    let v2 = v1.and_then(|v| Some(3));
+    assert_eq!(v2, Some(3));
+
+    let ans = reorder_spaces("  help".to_string());
+    print!("\"{}\"", ans);
+
+    let vec = vec![
+        String::from("zyc"),
+        String::from("zbz"),
+        String::from("zya"),
+    ];
+    longest_common_prefix(vec);
+
+    let a = [0i32, 1, 2];
+
+    let mut iter = a.iter().filter(|x| x.is_positive());
+
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), None);
+
+    println!("{:?}", a);
+
+    let string = String::from("abc");
+    let stringg: &str = &string;
+
+    stringg.clone();
+
+    let mut string2 = string.clone();
+
+    println!(
+        "string: {:p}, stringg: {:p}, string2: {:p}",
+        string.as_ptr(),
+        stringg,
+        string2.as_ptr()
+    );
+
+    // unsafe {
+    //     let string3 = String::from_raw_parts(0x55c1b3cb69d0 as *mut u8, 3, 3);
+    //     println!("string3 = {}", string3);
+    // }
+
+    {
+        use core::cell::Cell;
+
+        trait SomeTrait {
+            fn do_some(&self);
+        }
+
+        #[derive(Debug)]
+        struct S1 {
+            val: u32,
+        }
+
+        #[derive(Debug)]
+        struct S2 {
+            val: Cell<u32>,
+        }
+
+        impl SomeTrait for S1 {
+            fn do_some(&self) {
+                // I can't change anything.
+            }
+        }
+
+        impl SomeTrait for S2 {
+            fn do_some(&self) {
+                self.val.set(999);
+            }
+        }
+
+        let s1 = S1 { val: 111 };
+        let s2 = S2 {
+            val: Cell::new(222),
+        };
+
+        println!("{:?}, {:?}", s1, s2);
+
+        s1.do_some();
+        s2.do_some();
+
+        println!("{:?}, {:?}", s1, s2);
+
+        /*
+           S1 { val: 111 }, S2 { val: Cell { value: 222 } }
+           S1 { val: 111 }, S2 { val: Cell { value: 999 } }
+        */
+    }
 } // t1
+
+pub fn longest_common_prefix(strs: Vec<String>) -> String {
+    let d: String = strs
+        .iter()
+        .max()
+        .unwrap()
+        .chars()
+        .zip(strs.iter().min().unwrap().chars())
+        .take_while(|x| x.0 == x.1)
+        .map(|x| x.0)
+        .collect();
+    println!("{:?}", d);
+    return String::new();
+}
+
+struct Book {
+    a: [i8],
+}
+
+pub fn trim_mean(mut arr: Vec<i32>) -> f64 {
+    let dels = arr.len() / 20;
+    arr.sort();
+    (arr[dels..arr.len() - dels].iter().sum::<i32>()) as f64
+        / (arr.len() - dels * 2) as f64
+}
+
+// pub fn longest_common_prefix2(strs: Vec<String>) -> String {
+//     return strs
+//         .iter()
+//         .max()
+//         .zip(strs.iter().min())
+//         .take_while(|x| x.0 == x.1)
+//         .collect();
+// }
+
+// 1147
+pub fn longest_decomposition(text: String) -> i32 {
+    let len = text.len();
+    if len == 0 {
+        return 0;
+    }
+
+    for i in 1..len / 2 {
+        if text[0..i] == text[len - i - 1..len - i] {
+            return 2 + longest_decomposition(text[i..len - i - 1].to_string());
+        }
+    }
+
+    1
+}
+
+// 1624
+pub fn max_length_between_equal_characters(s: String) -> i32 {
+    s.chars()
+        .enumerate()
+        .map(|(i, x)| {
+            let len = s
+                .chars()
+                .enumerate()
+                .filter(|(_, y)| x == *y)
+                .map(|(j, _)| j as i32 - i as i32 - 1)
+                .max()
+                .or(Some(-1));
+            len.unwrap()
+        })
+        .max()
+        .or(Some(-1))
+        .unwrap() as i32
+}
+
+//1636
+pub fn frequency_sort(mut nums: Vec<i32>) -> Vec<i32> {
+    use std::collections::HashMap;
+
+    let mut freq = HashMap::new();
+
+    nums.iter().for_each(|x| {
+        match freq.get(x) {
+            Some(f) => freq.insert(x, f + 1),
+            None => freq.insert(x, 0),
+        };
+    });
+
+    let mut ans = nums.to_vec();
+
+    ans.sort_by(|a, b| {
+        if freq.get(a) == freq.get(b) {
+            b.cmp(a)
+        } else {
+            freq.get(a).cmp(&freq.get(b))
+        }
+    });
+    ans
+}
+
+//
+pub fn frequency_sort2(mut nums: Vec<i32>) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut count = HashMap::new();
+
+    for &num in &nums {
+        *count.entry(num).or_insert(0) += 1;
+    }
+
+    nums.sort_unstable_by(|a, b| b.cmp(a));
+    nums.sort_by_key(|k| count.get(k).unwrap());
+
+    nums
+}
+
+pub fn ttse() {
+    let strings = vec![String::from("abc"), String::from("123")];
+
+    strings
+        .iter()
+        .map(|x| {
+            //let d: () = x;  // &String
+        })
+        .count();
+
+    let mut string = String::from("ab");
+    // assert_eq!(Some('你'), string.chars().next());
+    string.chars().map(|mut x| x = 'c').count();
+    println!("{}", string);
+
+    // let chars = vec![''];
+    // chars.iter().map(|x| {
+    //     let d : () = x;
+    // });
+
+    // let ns = vec![1, 2, 3];
+    // ns.iter().map(|x| {
+    //     let d: () = x;
+    // });
+}
+
+#[test]
+fn testasd() {
+    ttse();
+}
